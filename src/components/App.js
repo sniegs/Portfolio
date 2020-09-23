c5613d5568ab0b820d37675bfe8d89eb5b90a466
Yuki tan <sniegs11@gmail.com>
2020-09-23 12:20:15 +0100

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import Title from './Title';
+import profile from '../assets/profile.png';
+import Jokes from './Jokes';
+
+class App extends Component {
+    state = {displayBio: false};
+
+    toggleDisplayBio = () =>{
+        this.setState({displayBio : !this.state.displayBio});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <img src={profile} alt='profile' className="profile"/>
+                <h1>Hello!</h1>
+                <p>My name is Arnold.</p>
+                <Title />
+                <p>I'm always looking forward to learning more and gaining more experience!</p>
+                {this.state.displayBio ?
+            (<div>
+                <p>I live in Nuneaton, and I am learning app development through Udemy courses.</p>
+                <p>Most of my experience in programming comes from C# and C++, but I am slowly moving towards JavaScript</p>
+                <p>Besides coding, I also love gaming and spending time with friends</p>
+                <button onClick = {this.toggleDisplayBio}>Show less</button>
+            </div>) : (
+                <div>
+                    <button onClick={this.toggleDisplayBio}>Read more</button>
+                </div>
+                )
+            }
+            <hr />
+            <Projects />
+            <hr />
+            <SocialProfiles />
+            <hr />
+            <Jokes />
+            </div>
+        )
+    }
+}
+
+export default App;
+
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,27 @@
+import emailIcon from '../assets/email_icon.png'
+import githubIcon from '../assets/github_icon.png'
+import linkedinlIcon from '../assets/linkedin_icon.png'
+import twitterIcon from '../assets/twitter_icon.png'
+
+
+const SOCIAL_PROFILES= [
+    {
+        id:1,
+        link:'mailto:manguzis.arnolds@gmail.com',
+        image:emailIcon
+    },
+
+    {
+        id:2,
+        link:'https://github.com/sniegs',
+        image:githubIcon
+    },
+
+    {
+        id:3,
+        link:'https://www.linkedin.com/in/arnolds-manguzis-42550bbb/',
+        image:linkedinlIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
