2cabb473ed20ed8353033ce8f97220f57c354101
mikelsoh <mikelsoh@uni.coventry.ac.uk>
2020-09-24 17:42:19 +0100

    Refactoring to use Async and await

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,35 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-const Joke = ({joke: {setup, punchline}}) =>(
-        <p style={{ margin:20}}>{setup} <em>{punchline}</em></p>
-    )
+const Joke = ({ joke: { setup, punchline } }) => (
+  <p style={{ margin: 20 }}>
+    {setup} <em>{punchline}</em>
+  </p>
+);
+
+const fetchJoke = async () => {
+  let response = await fetch(
+    "https://official-joke-api.appspot.com/random_joke"
+  );
+  let joke = await response.json();
+  return joke;
+};
+
+const fetchRandomJokes = async () => {
+  let response = await fetch(
+    "https://official-joke-api.appspot.com/random_ten"
+  );
+  let jokes = await response.json();
+  return jokes;
+};
 
 class Jokes extends Component {
-    state = {joke: {}, jokes: []};
-
-    componentDidMount(){
-        fetch('https://official-joke-api.appspot.com/random_joke')
-        .then(response => response.json())
-        .then(json => this.setState({joke: json}));
-    }
-
-    fetchJokes = () =>{
-        fetch('https://official-joke-api.appspot.com/random_ten')
-        .then(response => response.json())
-        .then(json => this.setState({jokes: json}));
-    }
-    render(){
-        return(
-            <div>
-                <h2>Highlighted Joke</h2>
-                <Joke joke={this.state.joke}/>
-                <hr/>
-                <h3>Want ten new Jokes?</h3>
-                <button onClick ={this.fetchJokes}>Click me!</button>
-                {this.state.jokes.map(joke =>( <Joke key={joke.id} joke={joke}/>))}
-            </div>
-        )
-    }
+  state = { joke: {}, jokes: [] };
+  getRandomJokes = async () => {
+    const json = await fetchRandomJokes();
+    this.setState({ ...this.state, jokes: json });
+  };
+  async componentDidMount() {
+    let joke = await fetchJoke();
+    this.setState({ ...this.state, joke: joke });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Highlighted Joke</h2>
+        <Joke joke={this.state.joke} />
+        <hr />
+        <h3>Want ten new Jokes?</h3>
+        <button onClick={this.getRandomJokes}>Click me!</button>
+        {this.state.jokes.map((joke) => (
+          <Joke key={joke.id} joke={joke} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Jokes;
