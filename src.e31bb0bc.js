f91aa07daacb83d5bed41f5db3ef515e2f8fd82e
Yuki tan <sniegs11@gmail.com>
2020-09-25 12:32:24 +0100

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -25656,7 +25656,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53278" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53645" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
